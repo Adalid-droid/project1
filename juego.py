57cf95de066e4122d37a93978eaaedd681c023b0
Adalid-droid <adalidfernando4296@gmail.com>
2025-01-30 23:41:10 -0400

    new file:   __pycache__/const.cpython-313.pyc 	deleted:    __pycache__/constants.cpython-310.pyc 	deleted:    __pycache__/constants.cpython-312.pyc 	deleted:    __pycache__/personaje.cpython-310.pyc 	deleted:    __pycache__/personaje.cpython-312.pyc 	new file:   __pycache__/personaje.cpython-313.pyc 	new file:   const.py 	deleted:    constants.py 	modified:   juego.py 	modified:   personaje.py

--- juego.py
+++ juego.py
@@ -1,19 +1,61 @@
 import pygame
-import constants
+import const
 import personaje
 from personaje import Personaje
 jugador = Personaje(50,50)
 
 pygame.init()
-ventana = pygame.display.set_mode((constants.ANCHO_VENTANA,constants.ALTO_VENTANA))
+ventana = pygame.display.set_mode((const.ANCHO_VENTANA,const.ALTO_VENTANA))
 run = True
 pygame.display.set_caption("mi primero juego")
-while run:
+#definir el estado de la tecla
+mover_arriba = False
+mover_abajo = False
+mover_izquieda = False
+mover_derecha = False
+#colocar el frame rate
+reloj = pygame.time.Clock()
+while run: 
+    #que vaya 60 fps
+    reloj.tick(const.FPS)
 
+    ventana.fill(const.COLOR_BG)
+    #calcular el movimiento del jugador
+    delta_x = 0
+    delta_y = 0
+    if mover_derecha == True:
+        delta_x = const.VELOCIDAD
+    if mover_izquieda == True:
+        delta_x = -const.VELOCIDAD
+    if mover_arriba == True:
+        delta_y = -const.VELOCIDAD
+    if mover_abajo == True:
+        delta_y = const.VELOCIDAD
+    #mover al jugador
+    jugador.movimiento(delta_x,delta_y)
     jugador.dibujar(ventana)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False 
+        if event.type== pygame.KEYDOWN:
+            if event.key ==  pygame.K_a:
+                mover_izquieda = True
+            if event.key ==  pygame.K_d:
+                mover_derecha = True
+            if event.key ==  pygame.K_w:
+                mover_arriba = True
+            if event.key ==  pygame.K_s:
+                mover_abajo = True
+        if event.type == pygame.KEYUP:
+            if event.key ==  pygame.K_a:
+                mover_izquieda = False
+            if event.key ==  pygame.K_d:
+                mover_derecha = False
+            if event.key ==  pygame.K_w:
+                mover_arriba = False
+            if event.key ==  pygame.K_s:
+                mover_abajo = False            
+
     pygame.display.update()
 pygame.quit()
--- personaje.py
+++ personaje.py
@@ -1,10 +1,15 @@
 import pygame
-import constants
+import const
 
 class Personaje():
     def __init__(self,x,y):
-        self.shape = pygame.Rect(0,0,constants.ANCHO_PERSONAJE,constants.ALTO_PERSONAJE)
+        self.shape = pygame.Rect(0,0,const.ANCHO_PERSONAJE,const.ALTO_PERSONAJE)
         self.shape.center = (x,y)
+    def movimiento(self, delta_x , delta_y):
+
+        self.shape.x  = self.shape.x +delta_x 
+        self.shape.y  = self.shape.y +delta_y 
 
     def dibujar(self, interfaz):
-        pygame.draw.rect(interfaz,(constants.COLOR_PERSONAJE),self.shape)
+        pygame.draw.rect(interfaz,(const.COLOR_PERSONAJE),self.shape)
+
